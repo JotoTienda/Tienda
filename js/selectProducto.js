73777d84cb07ce8747a9a4dea7f61e048744b476
XojeDev <jototienda@gmail.com>
2021-09-02 01:43:49 +0200

    I think complete

--- js/selectProducto.js
+++ js/selectProducto.js
@@ -3,9 +3,18 @@ import { disableAnchorChilds } from './xojeFunciones.js';
 const productos = document.querySelectorAll('.product');
 productos.forEach(producto => {
 	disableAnchorChilds(producto);
-	producto.onclick = (e=new Event("click")) => {
-		console.log(e.target);
-		const idProducto=producto.querySelector(".d-none");
-		//window.location.href=`http://fruitbenidorm.es/php/pages/product-single.php?id=${idProducto.innerText}`;
+	producto.onclick = (e = new Event("click")) => {
+
+		if (e.target.classList.contains("ion-ios-cart")) {
+			btn_add.onclick = () => {
+				const id = producto.querySelector(".d-none").innerText;
+				const precio = parseInt(producto.querySelector(".price-sale").innerText);
+				addProduct({ id, precio });
+			};
+		} else {
+			const idProducto = producto.querySelector(".d-none");
+
+			window.location.href = `http://fruitbenidorm.es/php/pages/product-single.php?id=${idProducto.innerText}`;
+		}
 	};
 });
