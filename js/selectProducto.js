0c369758acfaf5dca731fb5fe519530de24a9171
XojeDev <jototienda@gmail.com>
2021-09-02 01:34:40 +0200

    print event

--- js/selectProducto.js
+++ js/selectProducto.js
@@ -3,8 +3,8 @@ import { disableAnchorChilds } from './xojeFunciones.js';
 const productos = document.querySelectorAll('.product');
 productos.forEach(producto => {
 	disableAnchorChilds(producto);
-	producto.onclick = () => {
-		console.log(producto);
+	producto.onclick = (e) => {
+		console.log(e);
 		const idProducto=producto.querySelector(".d-none");
 		//window.location.href=`http://fruitbenidorm.es/php/pages/product-single.php?id=${idProducto.innerText}`;
 	};
