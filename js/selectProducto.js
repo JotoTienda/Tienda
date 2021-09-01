4a77ac27e221cc083cbf29a8b6fc5a2936e4293d
XojeDev <jototienda@gmail.com>
2021-09-02 01:30:19 +0200

    Firts steps for card with click sensitive

--- js/selectProducto.js
+++ js/selectProducto.js
@@ -4,6 +4,7 @@ const productos = document.querySelectorAll('.product');
 productos.forEach(producto => {
 	disableAnchorChilds(producto);
 	producto.onclick = () => {
+		console.log(producto);
 		const idProducto=producto.querySelector(".d-none");
 		window.location.href=`http://fruitbenidorm.es/php/pages/product-single.php?id=${idProducto.innerText}`;
 	};
