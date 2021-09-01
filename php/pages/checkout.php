c740b08e0b9f4a4fb7023dcc151bd7d05ef68343
XojeDev <jototienda@gmail.com>
2021-09-01 11:15:02 +0200

    Refector footer checkout

--- php/pages/checkout.php
+++ php/pages/checkout.php
@@ -168,28 +168,8 @@ require("../includes/navbar.php");
 	</div>
 </section> <!-- .section -->
 <?php
-require("includes/subNewsletter.php");
-require("includes/footer.php");
-require("includes/loader.php");
+require("../includes/main_footer.php");
 ?>
-<script src="../js/jquery.min.js"></script>
-<script src="../js/jquery-migrate-3.0.1.min.js"></script>
-<script src="../js/popper.min.js"></script>
-<script src="../js/bootstrap.min.js"></script>
-<script src="../js/jquery.easing.1.3.js"></script>
-<script src="../js/jquery.waypoints.min.js"></script>
-<script src="../js/jquery.stellar.min.js"></script>
-<script src="../js/owl.carousel.min.js"></script>
-<script src="../js/jquery.magnific-popup.min.js"></script>
-<script src="../js/aos.js"></script>
-<script src="../js/jquery.animateNumber.min.js"></script>
-<script src="../js/bootstrap-datepicker.js"></script>
-<script src="../js/scrollax.min.js"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-<script src="../js/google-map.js"></script>
-<script src="../js/main.js"></script>
-<script type="module" src="../js/xojeCheckout.js"></script>
-
 <script>
 	$(document).ready(function() {
 
@@ -226,7 +206,3 @@ require("includes/loader.php");
 
 	});
 </script>
-
-</body>
-
-</html>
