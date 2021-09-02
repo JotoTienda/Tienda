d533b619ab0b67d942f0678a0765c1fb22b48048
XojeDev <jototienda@gmail.com>
2021-09-02 16:17:18 +0200

    check modo_venta on venta

--- js/addProducto.js
+++ js/addProducto.js
@@ -1,13 +1,16 @@
-import { addProduct,disableAnchorChilds } from './xojeFunciones.js';
-const producto=document.querySelector(".product");
-const btn_add=producto.querySelector(".buy-now");
-const btn_cart=btn_add.querySelector(".ion-ios-cart");
+import { addProduct, disableAnchorChilds } from './xojeFunciones.js';
+const producto = document.querySelector(".product");
+const btn_add = producto.querySelector(".buy-now");
+const btn_cart = btn_add.querySelector(".ion-ios-cart");
 disableAnchorChilds(producto);
-btn_add.onclick=()=>{
+btn_add.onclick = () => {
     if (btn_cart) {
-        const id=producto.querySelector(".d-none").innerText;
-        const precio=parseInt(producto.querySelector(".price-sale").innerText);
-        addProduct({id,precio});
+        const id = producto.querySelector(".d-none").innerText;
+        const precio = parseInt(producto.querySelector(".price-sale").innerText);
+        const modo_venta=producto.querySelector(".price-sale").innerText.split('/')[1];
+        console.log(modo_venta);
+        alert("Stop");
+        addProduct({ id, precio });
     } else {
         window.location.href = "http://fruitbenidorm.es/php/pages/shop.php";
     }
