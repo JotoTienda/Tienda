89cb32ba97e2c4dd0e5f8eab59df08896f397bac
XojeDev <jototienda@gmail.com>
2021-09-02 18:55:11 +0200

    Try save de id on cookie when login

--- php/checkSession.php
+++ php/checkSession.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
 if (isset($_SESSION['idcliente'])) {
-	$query_login = 'SELECT * FROM clientes WHERE idcliente =' . $_SESSION['idcliente'];
+	$idCliente=$_SESSION['idcliente'];
+	$query_login = 'SELECT * FROM clientes WHERE idcliente =' . $idCliente;
 	$resultado = mysqli_query($con, $query_login);
 	$fila = mysqli_fetch_assoc($resultado);
 	$user = null;
