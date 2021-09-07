e1739a658b485b2acf56139b68cd43fbe93128bc
XojeDev <jototienda@gmail.com>
2021-09-07 14:33:01 +0200

    test loged

--- php/checkSession.php
+++ php/checkSession.php
@@ -3,13 +3,14 @@ session_start();
 require("conecta.php");
 
 if (isset($_SESSION['idcliente'])) {
-	$idCliente=$_SESSION['idcliente'];
-	$query_login = 'SELECT * FROM clientes WHERE idcliente ="' . $idCliente.'";';
+	$idCliente = $_SESSION['idcliente'];
+	$query_login = 'SELECT * FROM clientes WHERE idcliente ="' . $idCliente . '";';
 	$resultado = mysqli_query($con, $query_login);
 	$fila = mysqli_fetch_assoc($resultado);
 	$user = null;
 	if (count($fila) > 0) {
 		$user = $fila;
+		echo "<p>Sesion iniciada con" . $user["usuario"] . "</p>";
 	}
 	$isLogged = !empty($user);
 }
