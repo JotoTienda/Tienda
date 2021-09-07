d6cb2f6f4ff6be823656b970fe5838cb8390fedb
XojeDev <jototienda@gmail.com>
2021-09-07 09:26:59 +0200

    Test Session and cookie

--- php/pages/clientes.php
+++ php/pages/clientes.php
@@ -5,7 +5,9 @@ $isLogged = !empty($user);
 ?>
 <?php
 require("../includes/navbar.php");
+echo "<p>Session: " . $_SESSION['idcliente'] . " - Carrito " . $_COOKIE['carrito']["cliente"]["id"] . "</p>";
 ?>
+
 <?php if ($isLogged) : ?>
 	<br> Welcome. <?= $user['nombre']; ?>
 	<br>You are Successfully Logged In
