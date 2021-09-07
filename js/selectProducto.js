0c48990287368862ac86fee1dd13ae49ca6d3d48
XojeDev <jototienda@gmail.com>
2021-09-07 15:00:36 +0200

    correct if esle in selectProducto 2

--- js/selectProducto.js
+++ js/selectProducto.js
@@ -3,20 +3,20 @@ const productos = document.querySelectorAll(".product");
 productos.forEach((producto) => {
   disableAnchorChilds(producto);
   const btn_add = producto.querySelector(".buy-now");
-  btn_add.onclick = () => {
-    const idCliente = sessionStorage.getItem("idcliente");
-    if (idCliente) {
-      addProduct(producto);
-    }
-  };
+  btn_add.onclick = () => {};
   producto.onclick = (e = new Event("click")) => {
     if (!e.target.classList.contains("ion-ios-cart")) {
       if (e.target.classList.contains("ion-ios-log-in")) {
         window.location.href = `http://fruitbenidorm.es/php/pages/clientes.php`;
+      } else {
+        const idProducto = producto.querySelector(".d-none");
+        window.location.href = `http://fruitbenidorm.es/php/pages/product-single.php?id=${idProducto.innerText}`;
       }
     } else {
-      const idProducto = producto.querySelector(".d-none");
-      window.location.href = `http://fruitbenidorm.es/php/pages/product-single.php?id=${idProducto.innerText}`;
+      const idCliente = sessionStorage.getItem("idcliente");
+      if (idCliente) {
+        addProduct(producto);
+      }
     }
   };
 });
